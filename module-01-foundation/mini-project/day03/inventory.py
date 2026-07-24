5b62bc7b1d7ffc09e44a0d3f178696544a034a8c
Tariku Nigatu <tarikze2119@gmail.com>
2026-07-24 16:08:28 +0300

    edeited

--- module-01-foundation/mini-project/day03/inventory.py
+++ module-01-foundation/mini-project/day03/inventory.py
@@ -1,8 +1,8 @@
-# inventory.py
+
 
 stock = {}
 
-# Load stock from file
+
 try:
     with open("stock.txt", "r") as f:
         for line in f:
@@ -16,17 +16,17 @@ def adjust(item, amount):
     stock[item] = stock.get(item, 0) + amount
     print(f"{item} updated to {stock[item]}")
 
-# Example updates
+
 adjust("Paracetamol", 5)      # Increase stock
 adjust("Amoxicillin", -3)     # Decrease stock
 adjust("Vitamin C", 8)        # Add new item if it doesn't exist
 
-# Display all stock
+
 print("\nCurrent Stock:")
 for item, qty in stock.items():
     print(f"{item}: {qty}")
 
-# Report low-stock items
+
 print("\nLow Stock Items (less than 10):")
 low = [item for item, qty in stock.items() if qty < 10]
 
@@ -36,7 +36,7 @@ if low:
 else:
     print("No low-stock items.")
 
-# Save updated stock back to file
+
 with open("stock.txt", "w") as f:
     for item, qty in stock.items():
         f.write(f"{item},{qty}\n")
